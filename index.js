0ad998c8abec5ec244bde46d0c98cc1803080a5c
Jasterix <ljcalixte@gmail.com>
2019-06-12 16:09:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (objArray) => {
+  let obj = objArray.find((x) =>  {
+    return x.result === 'W'
+  })
+
+  return obj ? obj['year'] :obj
+}
